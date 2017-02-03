d38c1eab52b6805dc7a2b9289ff3c461feb76e56
zhaoxiaolin <zhaoxll@m15.cn>
2017-02-03 18:14:17 +0800

    update WelcomeActivity

--- app/src/main/java/app/m15/cn/goshopping/activity/WelcomeActivity.java
+++ app/src/main/java/app/m15/cn/goshopping/activity/WelcomeActivity.java
@@ -1,16 +1,29 @@
 package app.m15.cn.goshopping.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import app.m15.cn.goshopping.R;
 import app.m15.cn.goshopping.base.BaseActivity;
+import app.m15.cn.goshopping.util.CommonUtil;
 
 /**
  * Created by liueg on 2017/2/3.
  */
 
 public class WelcomeActivity extends BaseActivity {
+    class SkipRunnable implements Runnable{
+        private Context mContext;
+        public SkipRunnable(Context context){
+            mContext=context;
+        }
+        @Override
+        public void run() {
+            CommonUtil.startActivity(this.mContext,MainActivity.class);
+        }
+    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,10 @@ public class WelcomeActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        startMainActivity();
+    }
+
+    private void startMainActivity() {
+        new Handler().postDelayed(new SkipRunnable(this),3000);
     }
 }
