8eb6ecff6b92c176626468eb989e7aaf05ea3b2a
zhaoxiaolin <zhaoxll@m15.cn>
2017-02-03 15:29:00 +0800

    update ActivityTaskUtil

--- app/src/main/java/app/m15/cn/goshopping/util/ActivityTaskUtil.java
+++ app/src/main/java/app/m15/cn/goshopping/util/ActivityTaskUtil.java
@@ -1,8 +1,40 @@
 package app.m15.cn.goshopping.util;
 
+import android.app.Activity;
+
+import java.util.Stack;
+
 /**
  * Created by liueg on 2017/2/3.
  */
 
 public class ActivityTaskUtil {
+    public static ActivityTaskUtil sInstance;
+    private Stack<Activity> mActivityStack;
+
+    static {
+        sInstance = new ActivityTaskUtil();
+    }
+
+    private ActivityTaskUtil() {
+        mActivityStack = new Stack<>();
+    }
+
+    public static ActivityTaskUtil getInstance() {
+        return sInstance;
+    }
+
+    public void addActivity(Activity activity){
+        mActivityStack.add(activity);
+    }
+
+    public void removeActivity(Activity activity){
+        mActivityStack.remove(activity);
+    }
+
+    public void removeAllActivity(){
+         while (!mActivityStack.empty()){
+             mActivityStack.pop().finish();
+         }
+    }
 }
