9561b82266b1d31fc8eeeb693770c2ecc25fd64f
zhaoxiaolin <zhaoxll@m15.cn>
2017-02-03 17:19:04 +0800

    add WelcomeActivity

---
+++ app/src/main/java/app/m15/cn/goshopping/activity/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package app.m15.cn.goshopping.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import app.m15.cn.goshopping.R;
+import app.m15.cn.goshopping.base.BaseActivity;
+
+/**
+ * Created by liueg on 2017/2/3.
+ */
+
+public class WelcomeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
