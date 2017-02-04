3b95ff3c3a076fcb1943458d6439d56020833e90
zhaoxiaolin <zhaoxll@m15.cn>
2017-02-04 09:10:27 +0800

    update

--- app/src/main/java/app/m15/cn/goshopping/activity/MainActivity.java
+++ app/src/main/java/app/m15/cn/goshopping/activity/MainActivity.java
@@ -15,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+
     }
 }
---
+++ app/src/main/java/app/m15/cn/goshopping/view/MainViewIble.java
@@ -0,0 +1,8 @@
+package app.m15.cn.goshopping.view;
+
+/**
+ * Created by liueg on 2017/2/3.
+ */
+
+public interface MainViewIble {
+}
